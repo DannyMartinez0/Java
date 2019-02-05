8e314ee1f8ec8d293a2d67b8e5af8bf83b53a17c
DannyMartinez0 <quesitao@gmail.com>
2019-02-05 16:44:57 -0600

    prueba de danny

---
+++ ejercicios/src/texto/Variables.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package texto;
+
+/**
+ *
+ * @author danny
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte dato1 = 5;
+        byte dato2 = 10;
+        byte suma = (byte) (dato1 + dato2);
+        
+        int dato3 = 6;
+        int dato4 = 12;
+        int suma2 = dato3 + dato4;
+        
+        System.out.println("la suma es:."+(dato3+dato4));
+        
+    }
+    
+}
